3fd2e5c066e717be176120c0468a200a6b67ef6a
Elisa Damiani <101571666+elidami@users.noreply.github.com>
2024-03-22 11:09:20 +0100

    Modification for bash shell

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 pes_path=$(pwd)
 
 
-if !(grep -q ${pes_path} ~/.zshrc)
+if !(grep -q ${pes_path} ~/.bashrc)
 then
-echo "export PATH=${pes_path}/pes:\${PATH}" >> ~/.zshrc
+echo "export PATH=${pes_path}/pes:\${PATH}" >> ~/.bashrc
 else
-echo "pes already found on PATH. Please remove it from ~/.zshrc if you want to change the script location."
+echo "pes already found on PATH. Please remove it from ~/.bashrc if you want to change the script location."
 fi
 
 chmod +x pes/pes
